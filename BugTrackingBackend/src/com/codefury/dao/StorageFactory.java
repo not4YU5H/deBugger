a42cfe105633b9490cc8f61bbef65c5d3a2b0338
Sam Abraham <samabraham2022@gmail.com>
2024-08-18 15:28:40 +0530

    Architecture change

--- BugTrackingBackend/src/com/codefury/dao/StorageFactory.java
+++
@@ -1,9 +0,0 @@
-package com.codefury.dao;
-
-import java.sql.Connection;
-
-public class StorageFactory {
-    public static BugTrackingDao getConnection(){
-        return new BugTrackingDaoImpl();
-    }
-}
